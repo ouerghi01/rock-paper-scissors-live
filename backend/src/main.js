c0554016925f1177e48221a150f884739b94df90
ouerghi01 <mohamedaziz.ouerghi@etudiant-enit.utm.tn>
2025-01-03 00:21:51 +0100

    add some real time rendering

--- backend/src/main.js
+++ backend/src/main.js
@@ -9,42 +9,51 @@ const io = new Server(server);
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
 app.use(express.static(path.join(__dirname, 'public')));
-let users_online = [];
+let users_online = new Map();
 let users_not_available = [];
 io.on('connection', (socket) => {
     console.log('a user connected');
-    users_online.push(socket.id);
+    users_online.set(socket.id, "");
     socket.on('disconnect', () => {
-        users_online = users_online.filter((id) => id!== socket.id);  // remove the disconnected user from the list of users_online
+        users_online.delete(socket.id);
+        users_not_available = users_not_available.filter(id => id !== socket.id);
         console.log('user disconnected');
     });
-    socket.on('start_game', () => {
-        console.log("start game");
-        if(users_online.length >1) {
-          const randomIndex = Math.floor(Math.random() * users_online.length);
-          while(users_not_available.includes(randomIndex)){
-            console.log("randomIndex is not available");
-            randomIndex = Math.floor(Math.random() * users_online.length);
-          }
-          const randomUser = users_online[randomIndex];
-          
-        
-          io.to(randomUser).emit('start_game', {
-            message: `you are playing now with ${socket.id} player`,
-            player: socket.id
-          });
-          io.to(socket.id).emit('start_game', {
-            message: `you are playing now with ${randomUser} player`,
-            player: randomUser
-          });
-          users_not_available.push(randomUser);
-          users_not_available.push(socket.id);
-          
-        }
-        else {
-         
-          
-          socket.emit('waiting_for_player', "you are waiting for another player")
+    socket.on('start_game', (data) => {
+        users_online.set(socket.id, data.player);
+        console.log(data.player);
+        const available_users = Array.from(users_online.keys()).filter(id => !users_not_available.includes(id));
+        if (available_users.length > 0) {
+            let randomIndex = Math.floor(Math.random() * available_users.length);
+            let randomSocketId = available_users[randomIndex];
+            while (randomSocketId === socket.id) {
+                randomIndex = Math.floor(Math.random() * available_users.length);
+                randomSocketId = available_users[randomIndex];
+            }
+            const randomUser_name = users_online.get(randomSocketId);
+            const shared_session_id = Math.random().toString(36).substring(7);
+            io.sockets.sockets.get(randomSocketId).join(shared_session_id);
+            socket.join(shared_session_id);
+
+            io.to(shared_session_id).emit('start_game', {
+                message: `you are playing now !!`,
+                players: [
+                    {
+                        session_id: randomSocketId,
+                        name: "aziz"
+                    },
+                    {
+                        session_id: socket.id,
+                        name: "ahmed"
+                    }
+                ],
+                shared_session_id: shared_session_id,
+            });
+
+            users_not_available.push(randomSocketId);
+            users_not_available.push(socket.id);
+        } else {
+            socket.emit('waiting_for_player', "you are waiting for another player");
         }
     });
     socket.on('waiting_for_player', (msg) => {
